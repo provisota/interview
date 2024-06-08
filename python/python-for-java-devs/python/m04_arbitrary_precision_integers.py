13c41bc0f124cdc0831a506f8e3d0080d4f6d681
Ilia Alterovych <ilia.alterovych@kanexa.com>
2024-06-08 13:11:46 +0300

    added code snippets to `python-for-java-devs` package

---
+++ python/python-for-java-devs/python/m04_arbitrary_precision_integers.py
@@ -0,0 +1,10 @@
+lightyear_to_meter = 9_460_730_472_580_800
+min_milky_way_diameter = 170_000 * lightyear_to_meter
+
+min_milky_way_diameter_plus_one = min_milky_way_diameter + 1
+
+print("Woooooow, the milky way is at least", min_milky_way_diameter, "meters in diameter!")
+print("Adding one meter on that, we are at", min_milky_way_diameter_plus_one, "meters.")
+
+# > Woooooow, the milky way is at least 1608324180338736000000 meters in diameter!
+# > Adding one meter on that, we are at 1608324180338736000001 meters.
---
+++ python/python-for-java-devs/python/m04_boolean_expressions.py
@@ -0,0 +1,6 @@
+x = True
+y = False
+
+print("x or y =", x or y)    # logical or
+print("x and y =", x and y)  # logical and
+print("not x =", not x)      # logical not
